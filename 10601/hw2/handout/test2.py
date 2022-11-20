7c5a21aaccef24f051f14ca5310601ce42ea3e3a
zy963 <57026304+zy963@users.noreply.github.com>
2022-11-20 14:56:35 -0500

    Delete test2.py

--- 10601/hw2/handout/test2.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import math
-import operator
-
-file_path = 'C:\\Users\\Zheyi\\Documents\\CMU\\10601\\hw2\\handout\\small_train.tsv'
-dataset = np.genfromtxt(file_path, delimiter="\t", dtype=None, encoding=None)
-
-
